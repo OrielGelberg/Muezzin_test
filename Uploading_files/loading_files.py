9cce9f6e7158b21f2675a08e5069249374136235
oriel gelberg <g9093587@gmail.com>
2025-09-07 15:09:23 +0300

    fix work with json

--- Uploading_files/loading_files.py
+++ Uploading_files/loading_files.py
@@ -1,7 +1,5 @@
 import pathlib
-
 import datetime
-# from work_with_JSON import loading_JSON
 import json
 
 
@@ -12,30 +10,35 @@ class loading_files:
     def __init__(self):
         self.path = pathlib.Path(r"C:\Users\oriel\podcasts")
         self.data_dic = {}
-        # self.load_JSON = loading_JSON("metadata.json")
+        self.data_in_json = None
 
 
-    def loading_files(self):
+
+    def loading_file(self):
         for item in self.path.rglob("*.wav"):
             if item.is_file():
                 file_stats = item.stat()
                 metadata_dic = {
-                    "title": item.name,
-                    "Size": file_stats.st_size,
-                    "Created": datetime.datetime.fromtimestamp(file_stats.st_ctime),
-                    "inode": file_stats.st_ino,
-                    "dev": file_stats.st_dev,
+                    "title": str(item.name),
+                    "Size": str(file_stats.st_size),
+                    "Created": str(datetime.datetime.fromtimestamp(file_stats.st_ctime)),
+                    "inode": str(file_stats.st_ino),
+                    "dev": str(file_stats.st_dev),
                 }
-                self.data_dic["path"] = item
+                self.data_dic["path"] = str(item)
                 self.data_dic["metadata"] = metadata_dic
-                item_list = json.loads(self.data_dic)
 
-                # self.data_dic[item] = metadata_dic
-
-
+                self.data_in_json = json.dumps(self.data_dic,indent=4)
+                print(self.data_in_json)
 
 
 
 
 
 
+
+
+if __name__ == '__main__':
+    run = loading_files()
+
+    run.loading_file()
